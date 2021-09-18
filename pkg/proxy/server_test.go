5301380fd4a25329167baf2f2bd09b8f398c7bd1
Kent 'picat' Gruber <kent.picat.gruber@gmail.com>
2021-09-18 12:48:16 -0400

    Some minor cleanup to the proxy server test

--- pkg/proxy/server_test.go
+++ pkg/proxy/server_test.go
@@ -16,18 +16,32 @@ import (
 	"github.com/stretchr/testify/require"
 )
 
-func createTLSCACert(t *testing.T) (*x509.Certificate, *ecdsa.PrivateKey) {
+// createTLSCACert is a helper function that produces a test certificate authority
+// and corresponding private key, in this case using ECDSA.
+func generateNewSerialNumber(t *testing.T) *big.Int {
 	t.Helper()
-	// Create mTLS enabled upstream service listener
+
 	serialNumberLimit := new(big.Int).Lsh(big.NewInt(1), 128)
-	rand.Int(rand.Reader, serialNumberLimit)
+
 	serialNumber, err := rand.Int(rand.Reader, serialNumberLimit)
 	require.NoError(t, err)
 	require.NotNil(t, serialNumber)
 
+	return serialNumber
+}
+
+// createTLSCACert is a helper function that produces a test certificate authority (CA)
+// and corresponding private key, in this case using ECDSA.
+func createTLSCACert(t *testing.T) (*x509.Certificate, *ecdsa.PrivateKey) {
+	t.Helper()
+
+	// Generate a new serial number for the certificate
+	serialNumber := generateNewSerialNumber(t)
+
+	// Create a new CA certificate template
 	certTemplate := &x509.Certificate{
-		SerialNumber:          serialNumber,
 		IsCA:                  true,
+		SerialNumber:          serialNumber,
 		NotBefore:             time.Now(),
 		NotAfter:              time.Now().AddDate(10, 0, 0), // valid for 10 years
 		DNSNames:              []string{"ca"},
@@ -36,14 +50,17 @@ func createTLSCACert(t *testing.T) (*x509.Certificate, *ecdsa.PrivateKey) {
 		BasicConstraintsValid: true,
 	}
 
+	// Generate a new ECDSA private key
 	caCertPrivKey, err := ecdsa.GenerateKey(elliptic.P256(), rand.Reader)
 	require.NoError(t, err)
 	require.NotNil(t, caCertPrivKey)
 
+	// Create a CA certificate (self-signed) using the given template and ECDSA key material
 	caCertAns1DerBytes, err := x509.CreateCertificate(rand.Reader, certTemplate, certTemplate, &caCertPrivKey.PublicKey, caCertPrivKey)
 	require.NoError(t, err)
 	require.NotNil(t, caCertAns1DerBytes)
 
+	// Parse the created certificate to use it
 	caCert, err := x509.ParseCertificate(caCertAns1DerBytes)
 	require.NoError(t, err)
 	require.NotNil(t, caCert)
@@ -51,17 +68,15 @@ func createTLSCACert(t *testing.T) (*x509.Certificate, *ecdsa.PrivateKey) {
 	return caCert, caCertPrivKey
 }
 
+// createTLSServerCert is a helper function that produces a test server certificate
+// and corresponding private key, in this case using ECDSA.
 func createTLSServerCert(t *testing.T, caCert *x509.Certificate, caPrivKey *ecdsa.PrivateKey) (*x509.Certificate, *ecdsa.PrivateKey) {
 	t.Helper()
-	require.NotNil(t, caCert)
 
-	// Create mTLS enabled upstream service listener
-	serialNumberLimit := new(big.Int).Lsh(big.NewInt(1), 128)
-	rand.Int(rand.Reader, serialNumberLimit)
-	serialNumber, err := rand.Int(rand.Reader, serialNumberLimit)
-	require.NoError(t, err)
-	require.NotNil(t, serialNumber)
+	// Generate a new serial number for the certificate
+	serialNumber := generateNewSerialNumber(t)
 
+	// Create a new server certificate template
 	certTemplate := &x509.Certificate{
 		SerialNumber:          serialNumber,
 		NotBefore:             time.Now(),
@@ -72,14 +87,17 @@ func createTLSServerCert(t *testing.T, caCert *x509.Certificate, caPrivKey *ecds
 		BasicConstraintsValid: true,
 	}
 
+	// Generate a new ECDSA private key
 	serverCertPrivKey, err := ecdsa.GenerateKey(elliptic.P256(), rand.Reader)
 	require.NoError(t, err)
 	require.NotNil(t, serverCertPrivKey)
 
+	// Create a server certificate from the CA using the given template and ECDSA key material
 	serverCertAns1DerBytes, err := x509.CreateCertificate(rand.Reader, certTemplate, caCert, &serverCertPrivKey.PublicKey, caPrivKey)
 	require.NoError(t, err)
 	require.NotNil(t, serverCertAns1DerBytes)
 
+	// Parse the created certificate to use it
 	serverCert, err := x509.ParseCertificate(serverCertAns1DerBytes)
 	require.NoError(t, err)
 	require.NotNil(t, serverCert)
@@ -87,17 +105,16 @@ func createTLSServerCert(t *testing.T, caCert *x509.Certificate, caPrivKey *ecds
 	return serverCert, serverCertPrivKey
 }
 
+// createTLSServerCert is a helper function that produces a test client certificate
+// and corresponding private key, in this case using ECDSA.
 func createTLSClientCert(t *testing.T, caCert *x509.Certificate, caPrivKey *ecdsa.PrivateKey) (*x509.Certificate, *ecdsa.PrivateKey) {
 	t.Helper()
 	require.NotNil(t, caCert)
 
-	// Create mTLS enabled upstream service listener
-	serialNumberLimit := new(big.Int).Lsh(big.NewInt(1), 128)
-	rand.Int(rand.Reader, serialNumberLimit)
-	serialNumber, err := rand.Int(rand.Reader, serialNumberLimit)
-	require.NoError(t, err)
-	require.NotNil(t, serialNumber)
+	// Generate a new serial number for the certificate
+	serialNumber := generateNewSerialNumber(t)
 
+	// Create a new client certificate template
 	certTemplate := &x509.Certificate{
 		SerialNumber:          serialNumber,
 		NotBefore:             time.Now(),
@@ -108,14 +125,17 @@ func createTLSClientCert(t *testing.T, caCert *x509.Certificate, caPrivKey *ecds
 		BasicConstraintsValid: true,
 	}
 
+	// Generate a new ECDSA private key
 	clientCertPrivKey, err := ecdsa.GenerateKey(elliptic.P256(), rand.Reader)
 	require.NoError(t, err)
 	require.NotNil(t, clientCertPrivKey)
 
+	// Create a client certificate from the CA using the given template and ECDSA key material
 	clientCertAns1DerBytes, err := x509.CreateCertificate(rand.Reader, certTemplate, caCert, &clientCertPrivKey.PublicKey, caPrivKey)
 	require.NoError(t, err)
 	require.NotNil(t, clientCertAns1DerBytes)
 
+	// Parse the created certificate to use it
 	clientCert, err := x509.ParseCertificate(clientCertAns1DerBytes)
 	require.NoError(t, err)
 	require.NotNil(t, clientCert)
